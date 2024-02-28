1382a0d0190110ed05ea90652f1fb4eb0c0f1189
shams <chamsal-islem.bensaid@esprit.tn>
2024-02-28 23:14:29 +0100

    Ajout de l'interface AcheterProduit

--- src/main/java/edu/esprit/controllers/AcheterProduit.java
+++ src/main/java/edu/esprit/controllers/AcheterProduit.java
@@ -1,15 +1,16 @@
 package edu.esprit.controllers;
 
+import edu.esprit.entities.Panier;
 import edu.esprit.entities.Produit;
+import edu.esprit.entities.User;
+import edu.esprit.services.ServicePanier;
 import edu.esprit.services.ServiceProduit;
+import edu.esprit.services.ServiceUser;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.Labeled;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
-import java.util.Optional;
-import java.util.Set;
 
 public class AcheterProduit {
     @FXML
@@ -25,25 +26,45 @@ public class AcheterProduit {
     private Label publicationTime;
 
     @FXML
-    private ImageView imageProduit;
+    private Label descriptProduit;
+
+    @FXML
+    private Label nbrStock;
 
     @FXML
     private Label nomProduit;
 
     @FXML
-    private Label descriptProduit;
-
-    @FXML
     private Label prix;
 
     @FXML
-    private Label nbrStock;
+    private ImageView imageProduit;
+    private int produitId;
+
+    public void setProduitId(int produitId) {
+        this.produitId = produitId;
+    }
+
+    private MarketPlace marketPlaceController;
+
+    public void setMarketPlaceController(MarketPlace marketPlaceController) {
+        this.marketPlaceController = marketPlaceController;
+    }
+
+    private AjouterPanier ajouterPanierController;
+
+    // Méthode pour initialiser la référence au contrôleur AjouterPanier
+    public void setAjouterPanierController(AjouterPanier ajouterPanierController) {
+        this.ajouterPanierController = ajouterPanierController;
+    }
+
+
 
 
     ServiceProduit sp = new ServiceProduit();
 
 
-    public void initData(Image image, String userName, String publicationTime, String nomProduit, String prix, String nbrStock, String descriptProduit) {
+    public void initData(Image image, String userName, String publicationTime, String nomProduit, String prix, String nbrStock, String descriptProduit, int produitId) {
         this.imageProduit.setImage(image);
         this.userName.setText(userName);
         this.publicationTime.setText(publicationTime);
@@ -51,8 +72,27 @@ public class AcheterProduit {
         this.prix.setText(prix + " Par Pièce");
         this.nbrStock.setText(" Pièce(s) disponible : " + nbrStock);
         this.descriptProduit.setText(descriptProduit);
+        this.produitId = produitId;
 
     }
+
+    @FXML
+    public void ajouterPanier(ActionEvent actionEvent) {
+        ServiceUser serviceUser = new ServiceUser();
+        ServicePanier servicePanier = new ServicePanier();
+        User loggedInUser = serviceUser.authenticateUser("chams@gamail.com", "chams2000");
+
+        if (loggedInUser != null) {
+            Produit produit = sp.getOneByID(produitId);
+
+            if (produit != null) {
+                servicePanier.ajouterAuPanier(loggedInUser, produit);
+
+            }
+        }
+    }
+
+
 }
 
 
